d74f713348f9b47e9cb4352af206bcbbf70b5705
Anton-Patokin <patokin.anton@gmail.com>
2016-09-20 20:30:36 +0200

    hallo world

---
+++ angular2 learning/1-hallo world/dev/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>Angular 2 Boilerplate</h1>
+        <p>Hello World!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Accusantium adipisci aperiam at consequatur dolor, dolorem, error facere, 
+        fugiat impedit inventore iure magni nemo officiis 
+        perspiciatis sapiente sint veritatis vitae voluptatum?</p>
+    `,
+})
+export class AppComponent {
+
+}
