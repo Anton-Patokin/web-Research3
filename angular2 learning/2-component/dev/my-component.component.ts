35b853b3891bc8edcc45c3467a0656a929ec3e0b
Anton-Patokin <patokin.anton@gmail.com>
2016-09-20 20:45:28 +0200

    learn component and directives
    directives is a component with costum template

---
+++ angular2 learning/2-component/dev/my-component.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core'
+
+
+@Component({
+    selector:'my-component',
+    template: `<p>this is my component</p>
+    `,
+})
+
+export class MyComponentComponent{
+
+}
