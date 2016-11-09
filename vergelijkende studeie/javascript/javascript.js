cf42477876f48212e86afd69a643d8a2bc8df0b0
Anton-Patokin <patokin.anton@gmail.com>
2016-11-09 23:45:34 +0100

    vergelijken

--- vergelijkende studeie/javascript/javascript.js
+++ vergelijkende studeie/javascript/javascript.js
@@ -4,6 +4,15 @@ window.onload = function() {
     var obj = {
         name: "number"
     };
+    document.getElementById("add_watchers").addEventListener("click", my_numbers);
 
 
+    function my_numbers() {
+        for(var i =0; i<5000; i++){
+            var node = document.createElement("div");                 // Create a <li> node
+            var textnode = document.createTextNode(i);         // Create a text node
+            node.appendChild(textnode);                              // Append the text to <li>
+            document.getElementById("watchers").appendChild(node);
+        }
+    }
 };
